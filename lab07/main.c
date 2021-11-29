6e83c467df88ccf7dc524b44684b666402a8cae2
vlad <vladkremen@ginhub.com>
2021-11-30 01:14:56 +0200

    changes

--- lab07/main.c
+++ lab07/main.c
@@ -1,21 +1,22 @@
 #include <stdlib.h>
-int arr(int n, int x[]){
-  int k = 0;
-  for(int i = 0; i < 5; i++){
-    for(int j = i; j < 5; j++ ){
-      if (x[i] < x[j]) {
-        k++;
-      }
-    }
-    }
-  return k;
-}
+int arr(int n, int x[]);
 int main(){
-  int m = 5;
+  int m = 10;
   int X[m];
   for(int a = 0; a < m; a++){
     X[a] = rand() % 100;
   }
   int res = arr(m, X);
 return 0;
+}
+int arr(int n, int x[]){
+  int k = 0;
+  for(int i = 0; i < n; i++){
+    for(int j = i; j < n; j++ ){
+      if (x[i] < x[j]) {
+        k++;
+      }
+    }
+    }
+  return k;
 }
